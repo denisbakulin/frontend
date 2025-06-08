015dffa5bc9bdccac04895b6f6bd57901ac67f56
hordaaa <dtorkon@yandex.ru>
2025-06-08 20:36:38 +0700

    mn,m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "pages/Home/Home"
+import Auth from "pages/Auth/Auth"
+import {AuthProvider} from './context/AuthContext'
 
-function App() {
+
+import "assets/css/base.css"
+import "assets/css/vars.css"
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    
+    <BrowserRouter>
+        <AuthProvider>
+                <Routes>
+                    <Route path="/auth" element={<Auth />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+        </AuthProvider>
+    </BrowserRouter>
+  
 
-export default App;
+  )
+}
